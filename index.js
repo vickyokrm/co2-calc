7ca02149efc38c888deed210cb6a6b1497b134ff
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2021-05-24 16:10:33 +0200

    minor corrections

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ async function startApplication() {
     try {
         validate.userInput(args);
         const distanceBetweenCities = await distance.find(args.start, args.end);
-        const co2emisson = emissons.compute(distanceBetweenCities, args.transportationmethod);
-        console.log(`Your trip caused approx. ${co2emisson} kg of co2.`)
+        const co2= emissons.compute(distanceBetweenCities, args.transportationmethod);
+        console.log(`Your trip caused approx. ${co2} kg of co2.`)
     } catch (error) {
         console.error(error)
     }
