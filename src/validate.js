7034fb63f515b9de695cab8d15c21c32a5c4483f
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2021-05-24 17:00:44 +0200

    simplify variable names

--- src/validate.js
+++ src/validate.js
@@ -7,29 +7,29 @@ const raiseException = (msg) => {
     throw new Error(msg);
 }
 
-const userInput = (args) => {
-    if (args.hasOwnProperty('start')) {
-        if (typeof (args.start) !== "string" || args.start.length === 0) {
-            raiseException(`Empty or invalid start location: "${args.start}"`);
+const userInput = (start, end, transportationmethod) => {
+    if (start) {
+        if (typeof (start) !== "string" || start.length === 0) {
+            raiseException(`Empty or invalid start location: "${start}"`);
         }
     } else {
         raiseException('No start location found, use option --start')
     }
 
-    if (args.hasOwnProperty('end')) {
-        if (typeof (args.end) !== "string" || args.end.length === 0) {
-            raiseException(`Empty or invalid end location: "${args.end}"`);
+    if (end) {
+        if (typeof (end) !== "string" || end.length === 0) {
+            raiseException(`Empty or invalid end location: "${end}"`);
         }
     } else {
         raiseException('No destionation found, use option --end')
     }
 
-    if (args.hasOwnProperty('transportationmethod')) {
-        if (typeof (args.transportationmethod) !== "string" || args.transportationmethod.length === 0) {
-            raiseException(`Empty or invalid transportation method: "${args.transportationmethod}" `);
+    if (transportationmethod) {
+        if (typeof (transportationmethod) !== "string" || transportationmethod.length === 0) {
+            raiseException(`Empty or invalid transportation method: "${transportationmethod}" `);
         }
-        if(!allowedTransportMethods.hasOwnProperty(args.transportationmethod)) {
-            raiseException(`The given transport method is invalid: "${args.transportationmethod}" `);
+        if(!allowedTransportMethods.hasOwnProperty(transportationmethod)) {
+            raiseException(`The given transport method is invalid: "${transportationmethod}" `);
         }
     } else {
         raiseException('No transportation method found, use option --transportationmethod')
